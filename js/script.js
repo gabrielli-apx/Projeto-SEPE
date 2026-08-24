98444e8f0f712eca2e9f3c49b578ad545fcc2809
Gabrielli Aparecida Dias <gabrielli.dias1208@gmail.com>
2026-08-23 22:09:44 -0300

    inicio da linha do tempo e criacao do js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const navList = document.getElementById("navList");
 
-      const menuToggle = document.getElementById("menuToggle");
-      const navList = document.getElementById("navList");
-
-      menuToggle.addEventListener("click", () => {
-        const isOpen = navList.classList.toggle("open");
-
-        menuToggle.classList.toggle("active");
-
-        menuToggle.setAttribute("aria-expanded", isOpen);
-      });
+  menuToggle.addEventListener("click", () => {
+    const isOpen = navList.classList.toggle("open");
+    menuToggle.classList.toggle("active");
+    menuToggle.setAttribute("aria-expanded", isOpen);
+  });
+});
